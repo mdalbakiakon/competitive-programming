b40aece7a054f1bae5d3862d6f2b0ac7ca4b1e10
Md. Al Baki Akon <shahjadsiam512@gmail.com>
2026-08-16 18:16:41 +0600

    chore: adding class-27 and previous class will be added accordingly

---
+++ class-27/basics.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int a = 5;
+    double b = 2.75;
+
+    cout << a << "\n" << b << "\n";
+
+    int c;
+    double d;
+    cin >> c >> d;
+    cout << "value of c: " << c << "\n";
+    cout << "value of d: " << d << "\n";
+
+    return 0;
+}
