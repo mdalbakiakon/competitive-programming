6b09fcf371f6a05d2712f1618a518f578cf4a486
Md. Al Baki Akon <shahjadsiam512@gmail.com>
2026-09-14 18:49:54 +0600

    chore: learnt c++ overflow issue

--- withLUV/7.overflow.cpp
+++ withLUV/7.overflow.cpp
@@ -16,7 +16,45 @@ int main(){
 
 	// for integer remember 10 > 10 > 19 digits
 	// int > long > long long
+	// long acts differently on different OS
+	// so avoid it use long long and use int when needed
 
 	long long c = 1000000000;
 	cout << c*c << endl;
+
+	cout << endl;
+	cout << endl;
+	cout << endl;
+
+	int mx = INT_MAX;
+	cout << mx << endl;
+	cout << mx + 1 << endl; //overflowing
+
+	cout << endl;
+
+	int d = 100000;
+	int e = 100000;
+	long long int f = d * 1LL * e;
+	cout << f << endl;
+
+
+	double g = 1e24;
+
+	// it will print in scientific mode
+	cout << g << endl;
+
+	// fixed will remove scientific mode
+	cout << fixed << g << endl;
+
+	// setprecision(0) means how many digit we want after decimal
+	cout << fixed << setprecision(0) << g << endl;
+
+	// using double may contain very wide or big number but it has a cost
+	// the cost is for not accurate the digit u want to store
+	// so avoid double unless asked so
+	// use long long for int based large value as they are accurately stores
+
+	// still double not int or anything
+	cout << fixed << setprecision(0) << g << " " << typeid(g).name() << endl;
+
 }
